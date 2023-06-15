39b788c0321af3ee68977ec200fd67cfbcd3b450
Dmytro Solodkyi <dsolodky@gmail.com>
2023-06-15 12:07:57 +0300

    Added "ignoree" option

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
-
+const { program } = require("commander");
+const { version, description } = require("../package.json");
 const YAML = require("yaml");
 const process = require("process");
 const fs = require("fs/promises");
@@ -9,19 +10,22 @@ const _ = require("lodash");
 const envVarRegEx = /\$\{([a-zA-Z][a-zA-Z0-9_]+)\}/gm;
 const envVarKeysRegEx = /^\$\{([a-zA-Z][a-zA-Z0-9_]+)\}$/gm;
 
-function getEnv(envVarName) {
+function getEnv(envVarName, ignoreUnexisting) {
   const result = process.env[envVarName];
-  if (typeof result === "undefined")
-    throw new Error(`Envronment variable ${envVarName} is not defined`);
+  if (typeof result === "undefined") {
+    if (!ignoreUnexisting)
+      throw new Error(`Envronment variable ${envVarName} is not defined`);
+    result = "";
+  }
   return result;
 }
 
-function substituteDeep(obj) {
+function substituteDeep(obj, ignoreUnexisting) {
   if (Array.isArray(obj)) {
-    return obj.map((item) => substituteDeep(item));
+    return obj.map((item) => substituteDeep(item, ignoreUnexisting));
   } else if (typeof obj === "string") {
     return obj.replace(envVarRegEx, (match, envVarName) =>
-      substituteDeep(getEnv(envVarName))
+      substituteDeep(getEnv(envVarName, ignoreUnexisting), ignoreUnexisting)
     );
   } else if (typeof obj === "object") {
     const keys = _.keys(obj);
@@ -30,19 +34,22 @@ function substituteDeep(obj) {
       const matchKey = envVarKeysRegEx.exec(key);
       if (matchKey) {
         //key referes to env var, which is a peace of yaml
-        const yamlEnvVar = getEnv(matchKey[1]);
-        const additionalYaml = substituteDeep(YAML.parse(yamlEnvVar));
+        const yamlEnvVar = getEnv(matchKey[1], ignoreUnexisting);
+        const additionalYaml = substituteDeep(
+          YAML.parse(yamlEnvVar),
+          ignoreUnexisting
+        );
         result = _.merge(additionalYaml, result);
       } else {
         //key is a normal key
-        result[key] = substituteDeep(obj[key]);
+        result[key] = substituteDeep(obj[key], ignoreUnexisting);
       }
     }
     return result;
   } else return obj;
 }
 
-async function main(inputFileName, outputFileName) {
+async function main(inputFileName, outputFileName, ignoreUnexisting) {
   try {
     if (!inputFileName || !existsSync(inputFileName)) {
       throw new Error("Input file does not exists");
@@ -56,7 +63,7 @@ async function main(inputFileName, outputFileName) {
     let isFirstBlock = true;
     for (const block of blocks) {
       const data = YAML.parse(block);
-      const newData = substituteDeep(data);
+      const newData = substituteDeep(data, ignoreUnexisting);
       let lines = [...(isFirstBlock ? [] : ["---"]), YAML.stringify(newData)];
       await fs.appendFile(outputFileName, lines.join("\n"), {
         encoding: "utf8",
@@ -69,4 +76,14 @@ async function main(inputFileName, outputFileName) {
   }
 }
 
-main(process.argv[2], process.argv[3]);
+program
+  .name("yaml-envsubst")
+  .version(version)
+  .description(description)
+  .argument("<input file>", "Input yaml file")
+  .argument("<output file>", "Output yaml file")
+  .option("--ignore", "Ignore missing environment variables");
+
+program.parse();
+
+main(program.args[0], program.args[1], program.opts.ignore);
